30dc9ff7b40f54568d5f1cea0fb1b07523dde96d
Kouhei Maeda <mkouhei@palmtb.net>
2013-09-11 10:49:52 +0900

    fixes validate_token_with_policy
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- keystone/tests/test_api_v3_client.py
+++ keystone/tests/test_api_v3_client.py
@@ -1334,23 +1334,6 @@ class ApiV3ClientTests(unittest.TestCase):
         self.k.create_role(token=v.admin_token,
                            target_name=v.member_role_name)
 
-        """
-        self.k.grant_role_group_on_project(token=v.admin_token,
-                                           ou_name=v.x_project_name,
-                                           target_name=v.default_group_name,
-                                           role_name=v.member_role_name)
-
-        self.k.add_user_to_group(v.user01_userid,
-                                 token=v.admin_token,
-                                 group_name=v.default_group_name)
-                                 """
-        '''
-        self.k.grant_role_user_on_domain(token=v.admin_token,
-                                         ou_name=v.net_domain_name,
-                                         target_id=v.user01_userid,
-                                         role_name=v.member_role_name)
-                                         '''
-
         self.k.grant_role_user_on_project(token=v.admin_token,
                                           ou_name=v.x_project_name,
                                           target_id=v.user01_userid,
@@ -1360,15 +1343,14 @@ class ApiV3ClientTests(unittest.TestCase):
                                   v.user01_password,
                                   project_name=v.x_project_name,
                                   domain_name=v.net_domain_name)
-        print res.json()
-        print
+        print "authenticate:\n\n", res.json()
         subject_token = res.headers.get('x-subject-token')
-        #res = self.k.validate_token_with_policy(subject_token, v.admin_token)
-        res = self.k.validate_token(subject_token, v.admin_token)
+        res = self.k.validate_token_with_policy(subject_token, v.admin_token)
         self.assertEqual(200, res.status_code)
-        #self.assertTrue(res.json().get('allowed_actions'))
+        print "\nvalidate:\n\n", res.json()
+        self.assertTrue(res.json().get('allowed_actions'))
         res = self.k.list_services(token=subject_token)
-        print res
+        print "\nlist_service by menber:\n\n", res
         self.k.delete_role(token=v.admin_token,
                            target_name=v.admin_role_name)
         self.k.delete_role(token=v.admin_token,
@@ -1379,7 +1361,6 @@ class ApiV3ClientTests(unittest.TestCase):
         self.l.delete_entry(v.net_domain_name, 'domains')
         self.k.delete_policy(token=v.admin_token,
                              target_blob=v.policy_cinder)
-        #self.assertTrue(False)
 
     def test_member_of_user_on_project(self):
         # domain id must be same businessCategory
